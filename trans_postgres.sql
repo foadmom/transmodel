156fbbfe067e8e056c3eccc6b39e2e0c54cd9c60
foadm <foad_momtazi@hotmail.com>
2026-02-05 20:13:43 +0000

    debugging the functions

--- trans_postgres.sql
+++ trans_postgres.sql
@@ -7,27 +7,10 @@ DROP DATABASE IF EXISTS transmodel WITH (FORCE);
 CREATE DATABASE transmodel OWNER postgres;
 \c transmodel;
 
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS btree_gist;
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
+--DROP SCHEMA public;
 
---CREATE SCHEMA IF NOT EXISTS Common;
---CREATE SCHEMA IF NOT EXISTS reference;
---CREATE SCHEMA IF NOT EXISTS network;
---CREATE SCHEMA IF NOT EXISTS operation;
---CREATE SCHEMA IF NOT EXISTS tracking;
+ALTER ROLE postgres IN DATABASE transmodel SET search_path TO common;
 
-
---DROP TYPE IF EXISTS TRANSPORT_MODE ;
---DROP TYPE IF EXISTS STOP_TYPE ;
---DROP TYPE IF EXISTS SERVICE_DIRECTION ;
---DROP TYPE IF EXISTS COMPASS_DIRECTION;
---DROP TYPE IF EXISTS EXCEPTION_TYPE;
---DROP TYPE IF EXISTS REALTIME_STATUS ;
---DROP TYPE IF EXISTS JOURNEY_STATUS ;
---DROP TYPE IF EXISTS FUEL_TYPE ;
-
-\c transmodel
 CREATE TYPE TRANSPORT_MODE AS ENUM ('BUS','COACH','TRAIN');
 CREATE TYPE STOP_TYPE AS ENUM ('STATION','TERMINAL','STOP');
 CREATE TYPE SERVICE_DIRECTION AS ENUM ('OUTBOUND','INBOUND');
@@ -38,7 +21,19 @@ CREATE TYPE JOURNEY_STATUS AS ENUM ('PLANNED','IN_PROGRESS','COMPLETED','CANCELL
 CREATE TYPE FUEL_TYPE AS ENUM ('PETROL', 'DIESEL', 'GAS', 'ELECTRIC');
 
 
---ALTER ROLE postgres IN DATABASE transmodel;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+
+CREATE SCHEMA IF NOT EXISTS common;
+CREATE SCHEMA IF NOT EXISTS reference;
+CREATE SCHEMA IF NOT EXISTS network;
+CREATE SCHEMA IF NOT EXISTS operation;
+CREATE SCHEMA IF NOT EXISTS tracking;
+
+
+\c transmodel
 
 -- =========================================================
 -- ENUM Types
@@ -50,15 +45,20 @@ CREATE TYPE FUEL_TYPE AS ENUM ('PETROL', 'DIESEL', 'GAS', 'ELECTRIC');
 -- =========================================================
 -- =========================================================
 -- =========================================================
---DROP SCHEMA IF EXISTS reference;
-CREATE SCHEMA IF NOT EXISTS reference;
-ALTER ROLE postgres IN DATABASE transmodel SET search_path TO reference;
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+
 
 -- =========================================================
 -- =========================================================
 -- reference data, not all are reference
 -- =========================================================
-DROP TABLE IF EXISTS reference.stop;
+---- DROP TABLE IF EXISTS reference.stop;
+ALTER ROLE postgres IN DATABASE transmodel SET search_path TO reference;
 CREATE TABLE reference.stop (
     id                     BIGSERIAL PRIMARY KEY,
     atcocode               VARCHAR (16) NULL,
@@ -124,7 +124,7 @@ CSV HEADER;
 -- =========================================================
 -- 
 -- =========================================================
-DROP TABLE IF EXISTS reference.country;
+---- DROP TABLE IF EXISTS reference.country;
 CREATE TABLE reference.country (
     id        BIGSERIAL PRIMARY KEY,
     iso2      CHAR (2) NOT NULL,
@@ -415,7 +415,7 @@ INSERT INTO reference.country (iso2,latitude,longitude,"name",created_at,updated
 -- =========================================================
 -- 
 -- =========================================================
-DROP TABLE IF EXISTS reference.language;
+-- DROP TABLE IF EXISTS reference.language;
 CREATE TABLE IF NOT EXISTS reference.language (
     id BIGSERIAL PRIMARY KEY,
     ISO_639_2_Code_b CHAR(3) NOT NULL,
@@ -974,7 +974,7 @@ INSERT INTO reference."language" (iso_639_2_code_b,iso_639_2_code_t,iso_639_1_co
 -- =========================================================
 -- 
 -- =========================================================
-DROP TABLE IF EXISTS reference.city;
+-- DROP TABLE IF EXISTS reference.city;
 CREATE TABLE IF NOT EXISTS reference.city (
     id         BIGSERIAL PRIMARY KEY,
     name       VARCHAR (64) NOT NULL,
@@ -1289,7 +1289,7 @@ INSERT INTO reference.city ("name",latitude,longitude,country_id) VALUES
 -- =========================================================
 -- =========================================================
 --DROP SCHEMA IF EXISTS network CASCADE;
-CREATE SCHEMA IF NOT EXISTS network;
+--CREATE SCHEMA IF NOT EXISTS network;
 ALTER ROLE postgres IN DATABASE transmodel SET search_path TO network;
 
 
@@ -1327,22 +1327,22 @@ CREATE INDEX idx_zone_name ON network.zone (name);
 --   this means we do not get pin point accuracy of different
 --   eg platforms within a large stop like Victoria station
 -- =========================================================
-DROP TABLE IF EXISTS network.stop;
+-- DROP TABLE IF EXISTS network.stop;
 CREATE TABLE IF NOT EXISTS network.stop (
     id              BIGSERIAL PRIMARY KEY,
     code            VARCHAR (8) NOT NULL,
     name            VARCHAR (64),
-    type            STOP_TYPE NOT NULL,
+    type            public.STOP_TYPE NOT NULL,
     zone_id         BIGINT NOT NULL , 
     post_code       VARCHAR (16),
-    geo             geography,
+    geo             public.geography,
     additional_info VARCHAR (256),
     -- 
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now(),
     updated_by VARCHAR (32) DEFAULT CURRENT_USER
 );
-CREATE INDEX idx_stop_stop_code ON network.stop (stop_code);
+CREATE INDEX idx_stop_stop_code ON network.stop (code);
 CREATE INDEX idx_stop_post_code ON network.stop (post_code);
 CREATE INDEX idx_stop_zone_id ON network.stop (zone_id);
 --CREATE INDEX idx_stop_geo_position ON network.stop USING GIST (geo_position);
@@ -1441,7 +1441,7 @@ CREATE TABLE network.operator_network (
     id            BIGSERIAL PRIMARY KEY,
 	operator_id   BIGINT REFERENCES network.operator (id),
 	network_id    BIGINT REFERENCES network.network (id),
-    trans_mode    TRANSPORT_MODE NOT NULL,
+    trans_mode    public.TRANSPORT_MODE NOT NULL,
     country_id    BIGINT  NOT NULL REFERENCES reference.country (id),
     -- 
     created_at TIMESTAMP DEFAULT now(),
@@ -1473,7 +1473,7 @@ CREATE TABLE network.line (
     id          BIGSERIAL PRIMARY KEY,
     name        VARCHAR (128) NOT NULL,
     public_code VARCHAR (8),
-    trans_mode  TRANSPORT_MODE NOT NULL,
+    trans_mode  public.TRANSPORT_MODE NOT NULL,
     operator_id BIGINT  NOT NULL REFERENCES network.operator(id),
     --
     created_at TIMESTAMP DEFAULT now(),
@@ -1503,7 +1503,7 @@ CREATE TABLE network.service (
     line_id           BIGINT REFERENCES network.line(id),
     code              VARCHAR (8) UNIQUE NOT NULL,		-- eg FLX-241
 --    service_direction SERVICE_DIRECTION NOT NULL,
-    compass_direction COMPASS_DIRECTION,
+    compass_direction public.COMPASS_DIRECTION,
     from_stop_id      BIGINT REFERENCES network.stop(id),
     to_stop_id        BIGINT REFERENCES network.stop(id),
     distance_meters   INTEGER,
@@ -1693,7 +1693,7 @@ CREATE TABLE network.service_calendar (
 -- Real-Time Data (Optimized)
 -- =========================================================
 -- DROP SCHEMA IF EXISTS operation cascade;
-CREATE SCHEMA IF NOT EXISTS operation;
+-- CREATE SCHEMA IF NOT EXISTS operation;
 ALTER ROLE postgres IN DATABASE transmodel SET search_path TO operation;
 
 -- =========================================================
@@ -1701,13 +1701,13 @@ ALTER ROLE postgres IN DATABASE transmodel SET search_path TO operation;
 -- =========================================================
 CREATE TABLE IF NOT EXISTS operation.vehicle (
     id BIGSERIAL PRIMARY KEY,
-    code VARCHAR(8) NOT NULL, -- This may vary between different operators
+    code        VARCHAR(8) NOT NULL, -- This may vary between different operators
     registration VARCHAR (20) NOT NULL,
     vehicle_type VARCHAR (20),
-    capacity int NOT NULL,
-    seat_layout BIGINT, -- REFERENCES coach_seats_layout(id),
-    fuel FUEL_TYPE NOT NULL,
-    operator_id BIGINT REFERENCES network.operator(id),
+    capacity     INT NOT NULL,
+    seat_layout  BIGINT, -- REFERENCES coach_seats_layout(id),
+    fuel         public.FUEL_TYPE NOT NULL,
+    operator_id  BIGINT REFERENCES network.operator(id),
     -- 
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now(),
@@ -1737,7 +1737,7 @@ CREATE TABLE operation.vehicle_service_assignment (
 -- =========================================================
 CREATE TABLE operation.service_journey_status (
     service_journey_id BIGINT PRIMARY KEY REFERENCES network.service_instance(id),
-    status JOURNEY_STATUS,
+    status             public.JOURNEY_STATUS,
     -- 
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now(),
@@ -1751,11 +1751,11 @@ CREATE TABLE operation.service_journey_status (
 -- =========================================================
 -- 
 -- =========================================================
-CREATE SCHEMA IF NOT EXISTS tracking;
+-- CREATE SCHEMA IF NOT EXISTS tracking;
 CREATE TABLE tracking.vehicle_position (
     vehicle_id    BIGINT REFERENCES operation.vehicle(id),
     recorded_at   TIMESTAMP NOT NULL,
-    geo           geography,
+    geo           public.geography,
     speed         REAL,
     bearing       REAL,
     -- 
@@ -1777,7 +1777,7 @@ CREATE TABLE tracking.realtime_stop_update (
     delay_seconds       INTEGER,
     predicted_arrival_time   TIMESTAMP,
     predicted_departure_time TIMESTAMP,
-    status                   REALTIME_STATUS,
+    status                   public.REALTIME_STATUS,
     -- 
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now(),
@@ -1823,7 +1823,7 @@ CREATE INDEX idx_rt_update_recent ON tracking.realtime_stop_update (service_inst
 -- =========================================================
 -- 
 -- =========================================================
-CREATE SCHEMA IF NOT EXISTS common;
+-- CREATE SCHEMA IF NOT EXISTS common;
 
 -- =========================================================
 -- 
@@ -1914,9 +1914,16 @@ $$ LANGUAGE plpgsql;
 
 
 -- =========================================================
+-- =========================================================
+-- =========================================================
+-- =========================================================
+-- =========================================================
+-- =========================================================
+-- =========================================================
+-- =========================================================
 -- 
 -- =========================================================
-CREATE OR REPLACE FUNCTION network.insert_network (input json) RETURNS json AS $$
+CREATE OR REPLACE FUNCTION network.network_insert (input json) RETURNS json AS $$
     DECLARE
         _code TEXT := (input::json->>'code');
         _name TEXT := (input::json->>'name');
@@ -1930,6 +1937,8 @@ CREATE OR REPLACE FUNCTION network.insert_network (input json) RETURNS json AS $
 $$ LANGUAGE plpgsql;
 
 
+-- =========================================================
+-- =========================================================
 -- =========================================================
 -- 
 -- =========================================================
@@ -1954,10 +1963,47 @@ CREATE OR REPLACE FUNCTION network.operator_get_all () RETURNS json AS $$
     DECLARE
         _result json;
     BEGIN
-        select row_to_json (row) from (select * from network.operator) row;
+        select INTO _result row_to_json (row) from (select * from network.operator) row;
+		RETURN _result;
+--        select row_to_json (row) from (select * from network.operator) row;
+
     END;
 $$ LANGUAGE plpgsql;
 
+-- =========================================================
+-- =========================================================
+-- =========================================================
+-- example of the input:
+--   {"operator_code": "NEX", "network_code": "EN", "trans_mode":"COACH", "country_iso2":"GB"} 
+-- =========================================================
+CREATE OR REPLACE FUNCTION network.operator_network_insert (input json) RETURNS json AS $$
+    DECLARE
+        v_trans_mode TEXT := input::json->>'trans_mode';
+        v_country_iso2 TEXT := input::json->>'country_iso2';
+        v_operator_code TEXT := input::json->>'operator_code';
+        v_network_code TEXT := input::json->>'network_code';
+		_operator_id BIGINT;
+		_network_id BIGINT;
+		_trans_mode public.TRANSPORT_MODE = v_trans_mode::public.TRANSPORT_MODE;
+		_country_id BIGINT;
+    BEGIN
+		select into _operator_id (SELECT id FROM network.operator WHERE code=v_operator_code);
+		select into _network_id (SELECT id FROM network.network WHERE code=v_network_code);
+        SELECT INTO _country_id (SELECT id FROM reference.country WHERE iso2=v_country_iso2);
+        -- function body here
+	    INSERT INTO network.operator_network (operator_id, network_id, trans_mode, country_id) 
+			VALUES (_operator_id, _network_id, v_trans_mode, _country_id);
+--          VALUES     ((SELECT id FROM network.operator WHERE code=v_operator_code),
+--                     (SELECT id FROM network.network WHERE code=v_network_code), 
+--                     v_trans_mode::TRANSPORT_MODE, 
+--                     (SELECT id FROM reference.country WHERE iso2=v_country_iso2));
+
+        RETURN input;
+    END;
+$$ LANGUAGE plpgsql;
+
+SELECT common.function_wrapper ('network.operator_network_insert', 
+                   '{"operator_code": "NEX", "network_code": "EN", "trans_mode":"COACH", "country_iso2":"GB"}'::json);
 
 -- =========================================================
 -- =========================================================
@@ -1971,21 +2017,31 @@ $$ LANGUAGE plpgsql;
 -- =========================================================
 -- some test data
 -- =========================================================
-INSERT INTO network.operator (code, name) 
-    VALUES ('NEX', 'National Express'),
-           ('FLB', 'Flixbus');
-INSERT INTO network.network (code, name) 
-    VALUES ('UK', 'UK'),
-           ('SC', 'Scotland');
+ALTER ROLE postgres IN DATABASE transmodel SET search_path TO common;
+
+SELECT common.function_wrapper ('network.network_insert', '{"code": "EN", "name": "England"}'::json);
+SELECT common.function_wrapper ('network.network_insert', '{"code": "SC", "name": "Scotland"}'::json);
+
+
+SELECT common.function_wrapper ('network.operator_insert', '{"code": "NEX", "name": "National Express"}'::json);
+SELECT common.function_wrapper ('network.operator_insert', '{"code": "FLB", "name": "Flixbus"}'::json);
+SELECT common.function_wrapper ('network.operator_insert', '{"code": "EXL", "name": "Express Leisure Coaches"}'::json);
+
+
+SELECT common.function_wrapper ('network.operator_network_insert', 
+                   '{"operator_code": "NEX", "network_code": "EN", "trans_mode":"COACH", "country_iso2":"GB"}'::json);
 
 INSERT INTO network.operator_network (operator_id, network_id, trans_mode, country_id) 
       VALUES     ((SELECT id FROM network.operator WHERE code='NEX'),
             (SELECT id FROM network.network WHERE code='UK'), 'COACH', 
             (SELECT id FROM reference.country WHERE iso2='GB'));
-INSERT INTO network.operator_network (operator_id, network_id, trans_mode, country_id) 
-      VALUES     ((SELECT id FROM network.operator WHERE code='FLB'),
-            (SELECT id FROM network.network WHERE code='SC'), 'COACH', 
-            (SELECT id FROM reference.country WHERE iso2='GB'));
+
+
+
+--INSERT INTO network.operator_network (operator_id, network_id, trans_mode, country_id) 
+--      VALUES     ((SELECT id FROM network.operator WHERE code='FLB'),
+--            (SELECT id FROM network.network WHERE code='SC'), 'COACH', 
+--            (SELECT id FROM reference.country WHERE iso2='GB'));
 
 INSERT INTO network.line (name, public_code, trans_mode, operator_id )
         VALUES ('Birstol to London', 'BTL01', 'COACH', 1);
@@ -1993,13 +2049,17 @@ INSERT INTO network.line (name, public_code, trans_mode, operator_id )
 INSERT INTO network.zone (code, name) VALUES ('WMID', 'West Midlands'),
                                              ('LON',  'London');
 INSERT INTO network.stop (code, name, type, zone_id, post_code, geo) 
-        VALUES ('DGBT', 'Digbeth Coach Station',         'STATION', 1, 'B2',  'POINT(-1.888361  52.475453)'::geography),
-               ('LOVC', 'London Victoria Coach Station', 'STATION', 2, 'W12', 'POINT(-0.148277 -51.492525)'::geography);
+        VALUES ('DGBT', 'Digbeth Coach Station',         'STATION', 1, 'B2',  'POINT(-1.888361  52.475453)'::public.geography),
+               ('LOVC', 'London Victoria Coach Station', 'STATION', 2, 'W12', 'POINT(-0.148277 -51.492525)'::public.geography);
 
 INSERT INTO network.service (line_id, code, compass_direction, from_stop_id, to_stop_id)
         VALUES (1, 'BRLO', 'E', 1, 2);
+
+
+
 -- =========================================================
 -- =========================================================
+SELECT id FROM reference.country WHERE iso2='GB';
 SELECT common.function_wrapper ('network.insert_operator', '{"code": "EXL", "name": "Express Leisure Coaches"}'::json);
 
 select network.operator_get_all ();
@@ -2096,7 +2156,7 @@ select row_to_json (row) from (select * from network.operator) row;
 --Copy code
 --SELECT id, name
 --FROM stop_place
---ORDER BY geo_position <-> ST_MakePoint(:lon, :lat)::geography
+--ORDER BY geo_position <-> ST_MakePoint(:lon, :lat)::public.geography
 --LIMIT 5;
 
 
